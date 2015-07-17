677261d1e591614d97f22eece3f6d4b3eb06c104
blurryroots <blurryroots@posteo.de>
2015-07-17 18:23:46 +0200

    changing python test to include farnsworth

--- python/test.py
+++ python/test.py
@@ -7,7 +7,7 @@ class ZoundexTest(unittest.TestCase):
         pass
 
     def test_init(self):
-        self.assertEqual('F263', Zoundex("figsworth").encoded)
+        self.assertEqual('F652', Zoundex("farnsworth").encoded)
 
         self.assertEqual('L126', Zoundex("lovecraft").encoded)
         self.assertEqual('L126', Zoundex("loafkraft").encoded)
